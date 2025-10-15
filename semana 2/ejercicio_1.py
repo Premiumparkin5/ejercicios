1dc21d96c78df1d918d4a5d02227346c6cce10b6
Premiumparkin5 <dashgeometry1115@gmail.com>
2025-10-15 16:20:21 -0500

    Add files via upload

---
+++ semana 2/ejercicio_1.py
@@ -0,0 +1,9 @@
+nombre = "Samuel" #String
+edad = 17 #Int
+edad = "17"
+estatura = 1.75 #Float
+es_instructor = True #Booleano - lógico
+
+print (f"Nombre: {nombre}")
+print (f"Edad: {edad}")
+print (f"Estatura: {estatura}")
---
+++ semana 2/ejercicio_2.py
@@ -0,0 +1,19 @@
+#Condicionales
+edad = 18
+
+if edad < 18:
+    print ("Es menor de edad, no puede ingresar")
+elif edad == 18:
+    print ("Tienes la mayoria de edad, puedes ingresar")
+else:
+    print ("Tienes la mayoria de edad, puedes ingresar")
+
+usuario = "admin"
+contrasena = "Samy3210DiosAmado"
+
+if usuario == "admin" and contrasena == "Samy3210DiosAmado":
+    print ("Acceso concedido")
+else:
+    print ("Acceso Denegado") 
+
+
